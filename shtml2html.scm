60b0d7ea10dbb84d59a7e254aa9b3a4580a914b6
Peter Danenberg <pcd@wikitex.org>
2009-11-09 01:17:25 -0800

    eval; but let's take it out and do it right

--- shtml2html.scm
+++ shtml2html.scm
@@ -8,18 +8,27 @@
 
 (define default-doctype 'xhtml-1.1)
 
-(let ((doctype #f))
+(let ((doctype #f)
+      (do-eval #f))
   (let ((xhtml-1.1-option
          (option '("xhtml-1.1")
                  #f
                  #f
                  (lambda (option name arg seeds)
                    (set! doctype 'xhtml-1.1)
+                   seeds)))
+        (do-eval-option
+         (option '(#\e "eval")
+                 #f
+                 #f
+                 (lambda (option name arg seeds)
+                   (set! do-eval #t)
                    seeds))))
     (let ((args
            (args-fold
             (command-line-arguments)
-            (list xhtml-1.1-option)
+            (list xhtml-1.1-option
+                  do-eval-option)
             (lambda (option name arg seeds)
               (error "unrecognized option" name))
             cons
@@ -28,4 +37,21 @@
                               (assq doctype doctypes)
                               (assq default-doctype doctypes)))))
         (display doctype)
-        (write-shtml-as-html (read))))))
+        (if do-eval
+            ;; do-eval is expensive; might as well have, say, an fcgi
+            ;; server at this point. also, we don't have any support
+            ;; for error streams. on the other hand, it doesn't follow
+            ;; that i have to loop the read; what behooves it
+            ;; otherwise, though? main problem: need an absolute path
+            ;; to LOAD from; and syntax-case modules are too
+            ;; expensive.
+            ;;
+            ;; fuck it: just do a generic templating framework; keep
+            ;; this shit basic.
+            (let loop ((doc (read)))
+              (if (not (eof-object? doc))
+                  (let ((value (eval doc)))
+                    (if (pair? value)
+                        (write-shtml-as-html value))
+                    (loop (read)))))
+            (write-shtml-as-html (read)))))))
